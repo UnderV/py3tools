ab3beae27c8792801ed38b47c0ac61a8b1ba251e
UnderV <underv@gmail.com>
2015-04-16 17:18:04 +0300

    Create bash_exec.py

---
+++ bash_exec.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import subprocess
+
+def bash_exec(bashComand):
+    childProcess = subprocess.Popen(bashComand, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (out, err) = childProcess.communicate()
+    
+    # Return byte array converted to uft-8 string
+    return(out.decode("utf-8").strip(), err.decode("utf-8").strip(), childProcess.returncode)
