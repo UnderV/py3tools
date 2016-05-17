95e3490bb1ee5b8556cb7ffe9a604d2bf0893897
Andris Veldrajs <underv@gmail.com>
2016-05-17 21:23:18 +0000

    corrected for pep8

--- shell_exec.py
+++ shell_exec.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 import subprocess
 
-def shell_exec(bashComand):
-    childProcess = subprocess.Popen(bashComand, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+def shell_exec(bash_command):
+    childProcess = subprocess.Popen(bash_command,
+                                    shell=True,
+                                    stdin=subprocess.PIPE,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
     (out, err) = childProcess.communicate()
-    
+
     # Return byte array converted to uft-8 string
-    return(out.decode("utf-8").strip(), err.decode("utf-8").strip(), childProcess.returncode)
+    return(out.decode("utf-8").strip(),
+           err.decode("utf-8").strip(),
+           childProcess.returncode)
