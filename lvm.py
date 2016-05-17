edb1adf4c4ec1876d9a3fcdc327621e6322e0a0e
Andris Veldrajs <underv@gmail.com>
2016-05-17 21:10:51 +0000

    corrected for pep8

--- lvm.py
+++ lvm.py
@@ -17,13 +17,18 @@ class Lvm():
         if pv_return_code != 0:
             raise ValueError(pv_error, pv_return_code)
 
-        pv_output_line_array = pv_output.split('\n')  # Split output by 'newline'
-        pv_keys = pv_output_line_array[0].strip().split(';')  # First output line split by ';'
-        pv_output_line_array.pop(0)  # Delete first line from output array
+        # Split output by 'newline'
+        pv_output_line_array = pv_output.split('\n')
+        # First output line split by ';'
+        pv_keys = pv_output_line_array[0].strip().split(';')
+        # Delete first line from output array
+        pv_output_line_array.pop(0)
 
-        self.pv_dict = []  # Create dictionary array.
+        # Create dictionary array.
+        self.pv_dict = []
         for line in pv_output_line_array:
-            self.pv_dict.append(dict(zip(pv_keys, line.strip().split(";"))))  # Fill dictionary with key value
+            # Fill dictionary with key value
+            self.pv_dict.append(dict(zip(pv_keys, line.strip().split(";"))))
 
         # Getting data about Volume Groups
         (vg_output, vg_error, vg_return_code) = shell_exec("vgs"
@@ -35,26 +40,40 @@ class Lvm():
         if vg_return_code != 0:
             raise ValueError(vg_error, vg_return_code)
 
-        vg_output_line_array = vg_output.split('\n')  # Split output by 'newline'
-        vg_keys = vg_output_line_array[0].strip().split(';')  # First output line split by ';'
-        vg_output_line_array.pop(0)  # Delete first line from output array
+        # Split output by 'newline'
+        vg_output_line_array = vg_output.split('\n')
+        # First output line split by ';'
+        vg_keys = vg_output_line_array[0].strip().split(';')
+        # Delete first line from output array
+        vg_output_line_array.pop(0)
 
         self.vg_dict = []  # Create dictionary array.
         for line in vg_output_line_array:
-            self.vg_dict.append(dict(zip(vg_keys, line.strip().split(";"))))  # Fill dictionary with key value
+            # Fill dictionary with key value
+            self.vg_dict.append(dict(zip(vg_keys, line.strip().split(";"))))
 
         # Getting data about Logical Volumes
-        (lv_output, lv_error, lv_return_code) = shell_exec("lvs --units b --nosuffix --separator ';' -o lv_all 2>/dev/null")
+        (lv_output, lv_error, lv_return_code) = shell_exec("lvs"
+                                                           "--units b"
+                                                           "--nosuffix"
+                                                           "--separator ';'"
+                                                           "-o lv_all"
+                                                           "2>/dev/null")
         if lv_return_code != 0:
             raise ValueError(lv_error, lv_return_code)
 
-        lv_output_line_array = lv_output.split('\n')  # Split output by 'newline'
-        lv_keys = lv_output_line_array[0].strip().split(';')  # First output line split by ';'
-        lv_output_line_array.pop(0)  # Delete first line from output array
+        # Split output by 'newline'
+        lv_output_line_array = lv_output.split('\n')
+        # First output line split by ';'
+        lv_keys = lv_output_line_array[0].strip().split(';')
+        # Delete first line from output array
+        lv_output_line_array.pop(0)
 
-        self.lv_dict = []  # Create dictionary array.
+        # Create dictionary array.
+        self.lv_dict = []
         for line in lv_output_line_array:
-            self.lv_dict.append(dict(zip(lv_keys, line.strip().split(";"))))  # Fill dictionary with key value
+            # Fill dictionary with key value
+            self.lv_dict.append(dict(zip(lv_keys, line.strip().split(";"))))
 
         return True
 
@@ -64,91 +83,144 @@ class Lvm():
                 return True
         return False
 
-    def lv_exists(self, vg_name, lv_name):  # Check if Logical Volume exists
+    # Check if Logical Volume exists
+    def lv_exists(self, vg_name, lv_name):
         for item in self.lv_dict:
             if item["Path"] == '/dev/'+vg_name+'/'+lv_name:
                 return True
         return False
 
-    def get_vg_free_space(self, vg_name):  # Size in bytes
+    # Size in bytes
+    def get_vg_free_space(self, vg_name):
         for item in self.vg_dict:
             if item["VG"] == vg_name:
                 return item["VFree"]
         return False
 
-    def get_lv_attr(self, vg_name, lv_name):  # Get Logical Volume's attributes
+    # Get Logical Volume's attributes
+    def get_lv_attr(self, vg_name, lv_name):
         for item in self.lv_dict:
             if item["Path"] == '/dev/'+vg_name+'/'+lv_name:
                 return item["Attr"]
         return False
 
-    def get_vg_attr(self, vg_name):  # Get Volume Group's attributes
+    # Get Volume Group's attributes
+    def get_vg_attr(self, vg_name):
         for item in self.vg_dict:
             if item["VG"] == vg_name:
                 return item["Attr"]
         return False
 
-    def has_lv_snapshot(self, vg_name, lv_name):  # Check if Logical Volume has snapshots
+    # Check if Logical Volume has snapshots
+    def has_lv_snapshot(self, vg_name, lv_name):
         if self.lv_exists(vg_name, lv_name):
-            if self.get_lv_attr(vg_name, lv_name)[0] == 'o' or self.get_lv_attr(vg_name, lv_name)[0] == 'O':
+            if self.get_lv_attr(vg_name, lv_name)[0] == 'o' or \
+               self.get_lv_attr(vg_name, lv_name)[0] == 'O':
                 return True
         return False
 
-    def is_snapshot(self, vg_name, lv_name):  # Check if lv_name is snapshot
+    # Check if lv_name is snapshot
+    def is_snapshot(self, vg_name, lv_name):
         for item in self.lv_dict:
             if item["Path"] == '/dev/'+vg_name+'/'+lv_name:
                 if item["Origin"] and item["Attr"][0] == 's':
                     return True
         return False
 
-    def is_lv_in_use(self, vg_name, lv_name):  # Check if Logical Volume/Snapshot in use (for example - it is monted or opened)
-        if self.lv_exists(vg_name, lv_name):  # Check if Logical Volume exists
+    # Check if Logical Volume/Snapshot in use
+    # (for example - it is monted or opened)
+    def is_lv_in_use(self, vg_name, lv_name):
+        # Check if Logical Volume exists
+        if self.lv_exists(vg_name, lv_name):
             if self.get_lv_attr(vg_name, lv_name)[5] == 'o':
                 return True
         return False
 
-    def create_lv(self, vg_name, lv_name, lv_size):  # lv_size should be in bytes
-        if not isinstance(lv_size, str):  # Check if snap_size is string
-            lv_size = str(lv_size)  # Convert to string if variable is integer
+    # lv_size should be in bytes
+    def create_lv(self, vg_name, lv_name, lv_size):
+        # Check if snap_size is string
+        if not isinstance(lv_size, str):
+            # Convert to string if variable is integer
+            lv_size = str(lv_size)
 
-        if self.vg_exists(vg_name):  # Check if Volume Group exists
-            if int(self.get_vg_free_space(vg_name)) > int(lv_size):  # Check free space on Volume Group
-                if not self.lv_exists(vg_name, lv_name):  # Logical Volume should not exist
-                    (output, error, return_code) = shell_exec("lvcreate -L "+lv_size+"B -n "+lv_name+" /dev/"+vg_name)
+        # Check if Volume Group exists
+        if self.vg_exists(vg_name):
+            # Check free space on Volume Group
+            if int(self.get_vg_free_space(vg_name)) > int(lv_size):
+                # Logical Volume should not exist
+                if not self.lv_exists(vg_name, lv_name):
+                    (output, error, return_code) = shell_exec("lvcreate -L " +
+                                                              lv_size +
+                                                              "B -n " +
+                                                              lv_name +
+                                                              " /dev/" +
+                                                              vg_name)
                     if return_code == 0:
-                        self.reload()  # Read new data
+                        self.reload()
                         return True
         return False
 
-    def create_snapshot(self, vg_name, lv_name, snap_name, snap_size):  # snap_size should be in bytes, integer or string
-        if not isinstance(snap_size, str):  # Check if snap_size is string
-            snap_size = str(snap_size)  # Convert to string if variable is integer
+    # snap_size should be in bytes, integer or string
+    def create_snapshot(self, vg_name, lv_name, snap_name, snap_size):
+        # Check if snap_size is string
+        if not isinstance(snap_size, str):
+            # Convert to string if variable is integer
+            snap_size = str(snap_size)
 
-        if self.vg_exists(vg_name):  # Check if Volume Group exists
-            if int(self.get_vg_free_space(vg_name)) > int(snap_size):  # Check free space on Volume Group
-                if self.lv_exists(vg_name, lv_name):  # Check if Logical Volume exists
-                    if not self.lv_exists(vg_name, snap_name):  # Snapshot should not already exist
-                        (output, error, return_code) = shell_exec("lvcreate -L "+snap_size+"B -s -n "+snap_name+" /dev/"+vg_name+"/"+lv_name)
+        # Check if Volume Group exists
+        if self.vg_exists(vg_name):
+            # Check free space on Volume Group
+            if int(self.get_vg_free_space(vg_name)) > int(snap_size):
+                # Check if Logical Volume exists
+                if self.lv_exists(vg_name, lv_name):
+                    # Snapshot should not already exist
+                    if not self.lv_exists(vg_name, snap_name):
+                        (output, error, return_code) = shell_exec("lvcreate"
+                                                                  "-L " +
+                                                                  snap_size +
+                                                                  "B -s -n " +
+                                                                  snap_name +
+                                                                  " /dev/" +
+                                                                  vg_name +
+                                                                  "/" +
+                                                                  lv_name)
                         if return_code == 0:
-                            self.reload()  # Read new data
+                            self.reload()
                             return True
         return False
 
-    def delete_snapshot(self, vg_name, lv_name):  # lv_name is snapshot name
-        if self.is_snapshot(vg_name, lv_name):  # Check that Logical Volume is snapshot
-            if not self.is_lv_in_use(vg_name, lv_name):  # Logical Volume should not be in use
-                (output, error, return_code) = shell_exec("lvremove /dev/"+vg_name+"/"+lv_name+"  --force")
+    # lv_name is snapshot name
+    def delete_snapshot(self, vg_name, lv_name):
+        # Check that Logical Volume is snapshot
+        if self.is_snapshot(vg_name, lv_name):
+            # Logical Volume should not be in use
+            if not self.is_lv_in_use(vg_name, lv_name):
+                (output, error, return_code) = shell_exec("lvremove"
+                                                          "/dev/" +
+                                                          vg_name +
+                                                          "/" +
+                                                          lv_name +
+                                                          "  --force")
                 if return_code == 0:
-                    self.reload()  # Read new data
+                    self.reload()
                     return True
         return False
 
-    def delete_lv(self, vg_name, lv_name):  # Delete Logical Volume
-        if self.lv_exists(vg_name, lv_name):  # Check if Logical Volume exists
-            if not self.has_lv_snapshot(vg_name, lv_name):  # Logical Volume should have no snapshots
-                if not self.is_lv_in_use(vg_name, lv_name):  # Logical Volume should not be in use
-                    (output, error, return_code) = shell_exec("lvremove /dev/"+vg_name+"/"+lv_name+"  --force")
+    # Delete Logical Volume
+    def delete_lv(self, vg_name, lv_name):
+        # Check if Logical Volume exists
+        if self.lv_exists(vg_name, lv_name):
+            # Logical Volume should have no snapshots
+            if not self.has_lv_snapshot(vg_name, lv_name):
+                # Logical Volume should not be in use
+                if not self.is_lv_in_use(vg_name, lv_name):
+                    (output, error, return_code) = shell_exec("lvremove"
+                                                              "/dev/" +
+                                                              vg_name +
+                                                              "/" +
+                                                              lv_name +
+                                                              "  --force")
                     if return_code == 0:
-                        self.reload()  # Read new data
+                        self.reload()
                         return True
         return False
